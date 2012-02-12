278fef48bb433473589ee6701a21e3329f8d0bd3
Nicholas Devenish <n.devenish@gmail.com>
2012-02-12 13:24:09 +0000

    Add a basic admin interface for the MAC identities and nonces

---
+++ auth_mac/admin.py
@@ -0,0 +1,15 @@
+from auth_mac.models import Credentials, Nonce
+from django.contrib import admin
+
+
+class CredentialsAdmin(admin.ModelAdmin):
+  list_display = ['user', 'expiry', 'identifier', 'key' ]
+  # date_hierarchy = 'start'
+  ordering = ('user',)
+  # form = TokenForm
+
+class NonceAdmin(admin.ModelAdmin):
+  list_display = ['timestamp', 'credentials', 'nonce']
+
+admin.site.register(Credentials, CredentialsAdmin)
+admin.site.register(Nonce, NonceAdmin)

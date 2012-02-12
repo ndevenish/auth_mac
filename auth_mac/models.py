3b1c42b5001bf70fff47a53a1cf003538b619c53
Nicholas Devenish <n.devenish@gmail.com>
2012-02-12 01:06:21 +0000

    Add a basic model for the identifications

--- auth_mac/models.py
+++ auth_mac/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Credentials(models.Model):
+  "Keeps track of issued MAC credentials"
+  user = models.ForeignKey(User)
+  expiry = models.DateTimeField("Expires On")
+  identifier = models.CharField("MAC Key Identifier", max_length=16, null=True, blank=True)
+  key = models.CharField("MAC Key", max_length=16, null=True, blank=True)
+
+class Nonce(models.Model):
+  """Keeps track of any NONCE combinations that we have used"""
+  nonce = models.CharField("NONCE", max_length=16, null=True, blank=True)
+  timestamp = models.DateTimeField("Timestamp", auto_now_add=True)
+  credentials = models.ForeignKey(Credentials)

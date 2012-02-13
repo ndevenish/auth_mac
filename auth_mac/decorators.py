e284880bc85d0f44527ae38c38e37f1687a10987
Nicholas Devenish <n.devenish@gmail.com>
2012-02-13 23:20:05 +0000

    Create a required authorisation decorator, and wrap the protected view

--- auth_mac/decorators.py
+++ auth_mac/decorators.py
@@ -3,3 +3,56 @@ from django.contrib.auth.models import AnonymousUser
 from django.http import HttpResponse
 
 from auth_mac.models import Nonce, Credentials
+from auth_mac.tools import Validator
+
+def require_credentials(f):
+  @wraps(f)
+  def wrapper(request, *args, **kwargs):
+    """pull the credentials out of the request, and verify them"""
+    if not request.META.has_key("HTTP_AUTHORIZATION"):
+      response = HttpResponse(status=401)
+      response['WWW-Authenticate'] =  'MAC'
+      return response
+    # Build the validation object
+    authstr = request.META["HTTP_AUTHORIZATION"]
+    v = Validator(authstr)
+    v.is_valid()
+
+    return f(request, *args, **kwargs)
+  return wrapper
+
+
+# def extract(d, keys):
+#   "Extract a subset of a dictionary"
+#   return dict((k, d[k]) for k in keys if k in d)
+
+# def read_api_token(f):
+#   @wraps(f)
+#   def wrapper(request, *args, **kwargs):
+#     # If we have an API request, then prefer this auth
+
+#     # Extract the API information from the request
+#     authkeys = extract(request.REQUEST, ["client_key", "access_key", "nonce", "signature"])
+#     user = TokenAuthorisation().authenticate(**authkeys)
+#     # Replace the user if this worked, otherwise resume with the default
+#     if user:
+#       request.user = user
+    
+#     return f(request, *args, **kwargs)
+#   return wrapper
+
+# def require_api_token(f):
+#   @wraps(f)
+#   def wrapper(request, *args, **kwargs):
+#     # Extract the API information from the request
+#     authkeys = extract(request.REQUEST, ["client_key", "access_key", "nonce", "signature"])
+#     ta = TokenAuthorisation()
+#     user = ta.authenticate(**authkeys)
+#     if user is None:
+#       response = HttpResponse("Invalid API Token Authorisation:\n{0}".format(ta.errormessage),
+#                               status=401, content_type="text/plain")
+#       return response
+
+#     request.user = user
+#     return f(request, *args, **kwargs)
+#   return wrapper
--- auth_mac/tests/views.py
+++ auth_mac/tests/views.py
@@ -1,13 +1,16 @@
 from django.http import HttpResponse
 
+from auth_mac.decorators import require_credentials
+
 def unattainable_resource(request):
   "Always asks for authorisation."
   response = HttpResponse(status=401)
   response['WWW-Authenticate'] =  'MAC'
   return response
 
+@require_credentials
 def protected_resource(request):
   "Requires Authorisation"
-  response = HttpResponse(status=401)
-  response['WWW-Authenticate'] =  'MAC'
+  response = HttpResponse(status=200)
+  # response['WWW-Authenticate'] =  'MAC'
   return response

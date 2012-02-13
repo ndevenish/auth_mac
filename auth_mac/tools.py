030ef607a6ec6619a3a34167afb1d63126da3e70
Nicholas Devenish <n.devenish@gmail.com>
2012-02-13 01:52:27 +0000

    Rewrite the Signature class to work a bit smoother, and do validation

--- auth_mac/tools.py
+++ auth_mac/tools.py
@@ -9,48 +9,89 @@ def _build_authheader(method, data):
 class Signature(object):
   "A class to ease the creation of MAC signatures"
   MAC = None
+  data = {}
+  base_string = None
 
-  def __init__(self, credentials, host="example.com", port=80):
-    self.MAC = credentials
-    self.host = host
-    self.port = port
-    self.ext = ""
+  class SignatureError(Exception):
+    pass
   
-  def get_timestamp(self):
+  def __init__(self, credentials, **kwargs):
+    self.MAC = credentials
+    self.update_data_from_dictionary(kwargs)
+
+  def _add_data_item(self, from_dict, name, default=None):
+    """Basic inline function to add a key to the self data"""
+    if from_dict.has_key(name):
+      self.data[name] = from_dict[name]
+    else:
+      if not self.data.has_key(name):
+        self.data[name] = default
+  
+  def update_data_from_dictionary(self, from_dict):
+    "Read all required information out of a dictionary"
+    self._add_data_item(from_dict, "method", None)
+    self._add_data_item(from_dict, "uri", None)
+    self._add_data_item(from_dict, "host", None)
+    self._add_data_item(from_dict, "port", None)
+    self._add_data_item(from_dict, "ext", "")
+    self._add_data_item(from_dict, "timestamp", None)
+    self._add_data_item(from_dict, "nonce", None)
+    
+  def _get_timestamp(self):
     timestamp = datetime.datetime.utcnow() - datetime.datetime(1970,1,1)
     return timestamp.days * 24 * 3600 + timestamp.seconds
   
-  def get_nonce(self):
+  def _get_nonce(self):
     return User.objects.make_random_password(8)
 
-  def sign_request(self, uri, method="GET", timestamp=None, nonce=None):
+  def validate(self):
+    "Validates that we have all the required information"
+    if not self.MAC:
+      raise SignatureError("Have not been given a MAC credential")
+    required_values = {
+      "method": "HTTP Request Method",
+      "uri": "HTTP Request URI",
+      "host": "Destination Host",
+      "port": "Destination Port",
+      "ext": "Ext tag (default is \"\", you have deleted?",
+    }
+    # Check all of these
+    for key, errorstring in required_values.iteritems():
+      errorstring = "Missing information for signature: {0}".format(errorstring)
+      # If we don't have the key, or the key is None
+      if not self.data.has_key(key):
+        raise SignatureError(errorstring)
+      else:
+        if not self.data[key]:
+          raise SignatureError(errorstring)
+    # If the timestamp or nonce are blank, generate them
+    if not self.data["nonce"]:
+      self.data["nonce"] = self._get_nonce()
+    if not self.data["timestamp"]:
+      self.data["timestamp"] = self._get_timestamp()
+    # Make sure the method is capitalised
+    self.data["method"] = self.data["method"].upper()
+
+  def sign_request(self, **kwargs):
     """Signs a request to a specified URI and returns the signature"""
-    if not timestamp:
-      self.timestamp = self.get_timestamp()
-      timestamp = self.timestamp
-    if not nonce:
-      self.nonce = self.get_nonce()
-      nonce = self.nonce
-    self.nonce = nonce
-    self.timestamp = timestamp
-    method = method.upper()
-    if not method in ("GET", "POST"):
-      raise RuntimeError("HTTP Method {0} not supported!".format(method))
-    
-    data = [timestamp, nonce, method, uri, self.host, self.port, self.ext]
-    data = [str(x) for x in data]
+    self.update_data_from_dictionary(kwargs)
+    self.validate()
+    # What order do we use for calculations?
+    data_vars = ["timestamp", "nonce", "method", "uri", "host", "port", "ext"]
+    data = [str(self.data[x]) for x in data_vars]
     self.base_string = "\n".join(data) + "\n"
-    # print repr(basestr)
     # print "Signing with key '{0}'".format(self.MAC.key)
     hm = hmac.new(self.MAC.key, self.base_string, hashlib.sha1)
     self.signature = base64.b64encode(hm.digest())
     # print self.signature
     return self.signature
   
-  def get_header(self):
-    # {"id": "h480djs93hd8","ts": "1336363200","nonce":"dj83hs9s","mac":"bhCQXTVyfj5cmA9uKkPFx1zeOXM="}
+  def get_header(self, **kwargs):
+    "Return the HTTP Authorization header for the set IDs"
+    self.update_data_from_dictionary(kwargs)
+    self.validate()
     data = {"id": self.MAC.identifier, 
-            "ts": self.timestamp,
-            "nonce": self.nonce,
-            "mac": self.signature }
+            "ts": self.data["timestamp"],
+            "nonce": self.data["nonce"],
+            "mac": self.sign_request() }
     return _build_authheader("MAC", data)
